ea17d3276faba45572f0397dcdda230f8cf850fd
MrNaveent <126702769+MrNaveent@users.noreply.github.com>
2023-03-01 22:31:57 +0100

    Frontend Backend

---
+++ Web Applications/Qt LibraryApp/ASP.NET MVC (C#)LibraryApp  /Controllers/LibraryController.cs
@@ -0,0 +1,92 @@
+﻿using LibraryApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult EditBook(int b)
+        {
+
+            if (Request.HasFormContentType && Request.Form != null && Request.Form.Count > 0)
+            {
+                String a = HttpContext.Request.Form["a"].ToString();
+                if (dataHandler.getUser() != "librarian")
+                {
+                    return Redirect("/library");
+                }
+                Book loc = dataHandler.get().getBook(b);
+                if (a == "edit")
+                {
+                    string author = HttpContext.Request.Form["author"].ToString();
+                    int date = Int32.Parse(HttpContext.Request.Form["date"].ToString());
+                    string publisher = HttpContext.Request.Form["publisher"].ToString();
+                    string title = HttpContext.Request.Form["title"].ToString();
+                    string user = HttpContext.Request.Form["user"].ToString();
+                    b = Int32.Parse(HttpContext.Request.Form["b"].ToString());
+                    string status = HttpContext.Request.Form["status"].ToString();
+                    dataHandler.get().editBook(b, author, date, loc.leased, publisher, loc.reserved, title, user, status);
+
+                }
+
+            }
+            else if (b == 0)
+            {
+                return Redirect("/library");
+            }
+
+            ViewBag.b = b;
+
+            return View();
+        }
+
+        public IActionResult ReserveBook(int b)
+        {
+            dataHandler.get().changeBookStatus(b, "reserved", dataHandler.getUser());
+            return Redirect("/library");
+        }
+
+        public IActionResult Index(string myBooks = "off", string author = "", string title = "")
+        {
+            if(myBooks != "off" || author != "" || title != "")
+            {
+                if (title == null) title = "";
+                else title = title.ToLower();
+                if (author == null) author = "";
+                else author = author.ToLower();
+                List<Book> books = new List<Book>();
+                List<Book> allBooks = dataHandler.get().getBooks();
+         
+                foreach (Book book in allBooks)
+                {
+                    
+                    if((myBooks == "off" || book.user == dataHandler.getUser()) && book.author.ToLower().Contains(author) && book.title.ToLower().Contains(title) )
+                    {
+                        books.Add(book);
+                    }
+                }
+                ViewBag.myBooks = myBooks == "on";
+                ViewBag.author = author;
+                ViewBag.filterTitle = title;
+                return View(books);
+            }
+            else
+            {
+                return View(dataHandler.get().getBooks());
+            }
+            
+        }
+        public IActionResult Book(int b)
+        {
+            if (b == 0)
+            {
+                return Redirect("/library");
+            }
+            ViewBag.b = b;
+            return View();
+        }
+ 
+        
+    }
+}
---
+++ Web Applications/Qt LibraryApp/ASP.NET MVC (C#)LibraryApp  /Models/Book.cs
@@ -0,0 +1,89 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace LibraryApp.Models
+{
+    public struct Book
+    {
+       
+        public string user { get; set; }
+        public DateTime? reserved { get; set; } 
+        public DateTime? leased { get; set; }
+        public int id { get; set; }
+        public string author { get; set; }
+        public string title { get; set; }
+        public string publisher { get; set; }
+        public int date { get; set; }
+
+        public string toJson()
+        {
+            string ret = "";
+
+            ret += "        \"title\": \"" + title + "\",";
+            ret += "        \"user\": \"" + user + "\"";
+            ret += "        \"author\": \"" + author + "\",";
+            ret += "        \"date\": \"" + date + "\",";
+            ret += "        \"id\": \"" + id + "\",";
+            ret += "        \"leased\": \"" + leased + "\",";
+            ret += "        \"publisher\": \"" + publisher + "\",";
+            ret += "        \"reserved\": \"" + reserved + "\"";
+
+            return ret;
+        }
+
+        public bool print()
+        {
+            Console.WriteLine("book id: " + id + "\n author: " + author + "\n title: " + title +
+                "\n publisher: " + publisher + "\n date: " + date + "\n user: " + user +
+                "\n reserved: " + reserved + "\n leased: " + leased);
+            return true;
+        }
+
+        public Book(int id, string author, string title, string publisher, int date, string user, DateTime? reserved, DateTime? leased)
+        {
+            
+            this.user = user;
+            this.reserved = reserved;
+            this.leased = leased;
+            this.id = id;
+            this.author = author;
+            this.title = title;
+            this.publisher = publisher;
+            this.date = date;
+            
+        }
+       
+      
+        public void edit(string author, int date, DateTime? leased, string publisher, DateTime? reserved, string title, string user)
+        {
+            this.author = author;
+            this.date = date;
+            this.leased = leased;
+            this.publisher = publisher;
+            this.reserved = reserved;
+            this.title = title;
+            this.user = user;        
+        }
+       
+       
+             public string getStatus()
+        {
+            if(this.reserved == null)
+            {
+                this.leased = null;
+                return ("avaliable");
+            }
+            else
+            {
+                if(this.leased == null)
+                {
+                    return ("reserved");
+                }
+                else
+                {
+                    return ("leased");
+                }
+            }
+        }
+    }
+
+}
